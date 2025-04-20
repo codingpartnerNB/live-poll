c5216be5ef02e232912cc74e9376d714a143cfcb
codingpartnerNB <141160678+codingpartnerNB@users.noreply.github.com>
2025-04-20 22:14:27 +0530

    Update vite.config.js

--- front-poll/vite.config.js
+++ front-poll/vite.config.js
@@ -1,20 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 3000,
-//     proxy: {
-//       '/api': {
-//         target: 'http://localhost:5000',
-//         changeOrigin: true
-//       }
-//     }
-//   }
-// })
-
-
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
@@ -24,12 +7,13 @@ export default defineConfig({
   server: {
     proxy: {
       '/socket.io': {
-        target: 'http://localhost:5000',
+        // target: 'http://localhost:5000',
+        target: 'https://poll-spark-backend.onrender.com',
         ws: true, // Enable WebSocket proxying
         changeOrigin: true,
       },
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://poll-spark-backend.onrender.com',
         changeOrigin: true,
       },
     },
