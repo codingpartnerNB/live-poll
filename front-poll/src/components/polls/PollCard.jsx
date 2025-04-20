3e340104d55e7275ad75ae4300f834de77fce03c
codingpartnerNB <141160678+codingpartnerNB@users.noreply.github.com>
2025-04-20 23:07:36 +0530

    Update PollCard.jsx

--- front-poll/src/components/polls/PollCard.jsx
+++ front-poll/src/components/polls/PollCard.jsx
@@ -132,7 +132,7 @@ const PollCard = ({ poll, handleDelete }) => {
           {/* Action button */}
           <button
             onClick={handleViewDetails}
-            className="relative inline-flex items-center justify-center w-full px-4 py-2.5 overflow-hidden font-medium transition-all duration-300 ease-out rounded-lg group bg-gradient-to-r from-purple-600 to-blue-500 hover:from-purple-500 hover:to-blue-400"
+            className="relative inline-flex items-center justify-center cursor-pointer w-full px-4 py-2.5 overflow-hidden font-medium transition-all duration-300 ease-out rounded-lg group bg-gradient-to-r from-purple-600 to-blue-500 hover:from-purple-500 hover:to-blue-400"
           >
             <span className="absolute bottom-0 right-0 w-8 h-2 -mb-1 -mr-1 transition-all duration-500 ease-out transform rotate-45 translate-x-1 bg-white/30 group-hover:-translate-x-2"></span>
             <span className="absolute top-0 left-0 w-8 h-2 -mt-1 -ml-1 transition-all duration-500 ease-out transform -rotate-45 -translate-x-1 bg-white/30 group-hover:translate-x-2"></span>
