cdd7f0aef1ae24629e44a2077cf303d4e22deed0
codingpartnerNB <141160678+codingpartnerNB@users.noreply.github.com>
2025-04-20 22:17:58 +0530

    Update CreatePoll.jsx

--- front-poll/src/pages/CreatePoll.jsx
+++ front-poll/src/pages/CreatePoll.jsx
@@ -24,7 +24,7 @@ const CreatePoll = () => {
     setLoading(true);
     
     try {
-      const res = await axios.post('http://localhost:5000/api/polls', 
+      const res = await axios.post('https://poll-spark-backend.onrender.com/api/polls', 
         pollData,
         { headers: { Authorization: `Bearer ${user.token}` } }
       );
